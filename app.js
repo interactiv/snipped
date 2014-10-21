a5a3f1f36ca5bb968724dd978f68ce20d6ee5738
mparaiso <aikah@free.fr>
2014-10-21 13:18:37 +0200

    update Tue Oct 21 11:18:37 GMT 2014

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 /*jslint eqeq:true,node:true,white:true,plusplus:true,nomen:true,unparam:true,devel:true,regexp:true */
-/**
-* Module dependencies.
-*/
 "use strict";
-/*
-var express = require('express')
-    , http = require('http')
-    , path = require('path')
-
-var app = express();
-    // all environments
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-*/
 var container = require('./lib/container'),
     http =require('http');
 
 module.exports = container;
 if (!module.parent) {
-    http.createServer(container.get('app')).listen(container.get('port'), function(){
+    http.createServer(container.get('app')).listen(container.get('port'),container.get('ip'), function(){
         console.log('server listening on port ' + container.get('port'));
     });
 }
